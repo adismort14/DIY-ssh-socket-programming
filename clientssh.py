3327f6d951a77a9308d9faeb67c7d55a36de09b5
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2023-11-13 12:38:58 +0530

    finished the clientssh.py file

---
+++ clientssh.py
@@ -0,0 +1,43 @@
+from socket import *
+import sys
+
+sname="10.10.64.179"
+sport=8000
+
+csock=socket(AF_INET,SOCK_STREAM)
+csock.connect((sname,sport))
+
+while(True):
+    usrname=input("Enter the server username: ")
+    csock.send(usrname.encode())
+    susr_res=csock.recv(2048)
+    print(susr_res)
+    if(susr_res.decode()=='1'):
+        while(True):
+            passwd=input("Enter the server password: ")
+            csock.send(passwd.encode())
+            spass_res=csock.recv(2048)
+            if(spass_res.decode()=='1'):
+                print(f"You have now been connected to @{usrname}")
+                break
+            else:
+                print("The password you entered is wrong. Please try again.")
+        break
+    else:
+        print("The given username does not exist, please try again.")
+
+cmd=input("Enter any UNIX command (ls,pwd,cd,echo etc). Enter 'exit' to close the connection: ")
+
+while cmd!="exit":
+    csock.send(cmd.encode())
+    cont=csock.recv(2048)
+    print(cont.decode())
+
+    cmd=input("Enter another UNIX command.Enter 'exit' to close the connection: ")
+    if cmd=="exit":
+        break
+csock.close()
+
+if(cmd=="exit"):
+    csock.close()
+    sys.exit()
